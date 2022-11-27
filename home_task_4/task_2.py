c3c6876a86ae14e39566385b23d41f32ed7b80bd
Peter Orlov <peter_orl@mail.ru>
2022-11-27 17:57:45 +0300

    task_2

--- home_task_4/task_2.py
+++ home_task_4/task_2.py
@@ -7,5 +7,8 @@ port=1883
 theme="task_2"
 mqtt_dir="C:\Program Files\mosquitto"
 os.chdir(mqtt_dir)
-os.system("mosquitto_sub -h "+brocker+" -t "+theme+" -C 3")
-print(subprocess.getoutput())
+while(True):
+    mqtt_get=subprocess.getoutput("mosquitto_sub -h " + brocker + " -t " + theme + " -C 3")
+    v=[float(i) for i in mqtt_get.splitlines()]
+    movingAverage=0.6*v[2]+0.3*v[1]+0.1*v[0]
+    print("Moving average: ", movingAverage)
