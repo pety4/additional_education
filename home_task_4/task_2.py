0b89801d2e8fd35a0028e8094ca8eb668d38e14e
Peter Orlov <peter_orl@mail.ru>
2022-12-16 15:59:13 +0300

    practice_2

--- home_task_4/task_2.py
+++ home_task_4/task_2.py
@@ -1,4 +1,3 @@
-import paho.mqtt as mqtt
 import os
 import subprocess
 
@@ -7,8 +6,10 @@ port=1883
 theme="task_2"
 mqtt_dir="C:\Program Files\mosquitto"
 os.chdir(mqtt_dir)
+v=[0,0,0]
 while(True):
-    mqtt_get=subprocess.getoutput("mosquitto_sub -h " + brocker + " -t " + theme + " -C 3")
-    v=[float(i) for i in mqtt_get.splitlines()]
-    movingAverage=0.6*v[2]+0.3*v[1]+0.1*v[0]
+    mqtt_get=float(subprocess.getoutput("mosquitto_sub -h " + brocker + " -t " + theme + " -C 1"))
+    v.pop(2)
+    v.insert(0,mqtt_get)
+    movingAverage=0.6*v[0]+0.3*v[1]+0.1*v[2]
     print("Moving average: ", movingAverage)
--- practice_1/botcontrol.py
+++ practice_1/botcontrol.py
@@ -1,4 +1,4 @@
-#создать отдельные функции
+# создать отдельные функции
 
 import sys
 import os
@@ -7,92 +7,89 @@ import numpy as np
 import math
 import paho.mqtt.client as mqtt
 
+
 def on_connect(client, userdata, flags, rc):
-    if rc==0:
-        client.connected_flag=True
+    if rc == 0:
+        client.connected_flag = True
         print("connected OK")
     else:
         print("Bad connection Returned code=", rc)
 
+
 class Bot():
-    def __init__(self, coords, v=0, omega=0):
-        self.coords = coords
-        self.currentAngle = 0.0
+    message = """'cmd':'{}''val':'{}'"""
+    def __init__(self, v=2.0, omega=2.0,
+                 mqtt_ip="127.0.0.1", mqtt_port=1883, mqtt_theme="practice_1", filepath="path.txt"):
+        self.currentAngle = 0
         self.v = v
         self.omega = omega
+        self.mqtt_ip = mqtt_ip
+        self.mqtt_port = mqtt_port
+        self.mqtt_theme = mqtt_theme
+        self.file_path = filepath
+        if not os.path.exists(self.file_path):
+            raise FileExistsError("File not found")
+        else:
+            self.path = np.loadtxt(self.file_path, dtype=float)
+        self.move()
 
-    def distance(self, newCoords):
+    def distanceCount(self, newCoords):
         return math.sqrt((newCoords[0] - self.coords[0]) ** 2 + (newCoords[1] - self.coords[1]) ** 2)
 
-    def angle(self, newCoords):
-        return (math.degrees(
+    def angleCount(self, newCoords):
+        def alphaCheck(alpha):
+            if alpha>=360:
+                alpha-=360
+            elif alpha<=-360:
+                alpha+=360
+            return math.fabs(alpha)
+        return alphaCheck(math.degrees(
             math.atan2(newCoords[1] - self.coords[1], newCoords[0] - self.coords[0])) - self.currentAngle)
-
+    def move(self):
+        def forward(distance):
+            cmd="forward"
+            val=distance/self.v
+            commandPublish(cmd, val)
+        def back(distance):
+            cmd="back"
+            val=distance/self.v
+            commandPublish(cmd, val)
+        def right(angle):
+            cmd = "right"
+            val = abs(angle) / self.omega
+            commandPublish(cmd, val)
+        def left(angle):
+            cmd="left"
+            val=angle/self.omega
+            commandPublish(cmd,val)
+        def commandPublish(cmd,val):
+            client.publish(self.message.format(cmd, val))
+            time.sleep(val)
+        mqtt.Client.connected_flag = False
+        client = mqtt.Client("practice_1")
+        client.on_connect = on_connect
+        print("Connecting to brocker ", self.mqtt_ip)
+        client.connect(self.mqtt_ip, self.mqtt_port)
+        print("start loop")
+        for i in range(1,len(self.path)):
+            self.coords = self.path[i-1]
+            angle=self.angleCount(self.path[i])
+            distance=self.distanceCount(self.path[i])
+            if angle==0:
+                forward(distance)
+            elif 0<angle<180:
+                left(angle)
+                forward(distance)
+            elif angle==180:
+                back(distance)
+            elif 180<angle<359:
+                right(angle)
+                forward(distance)
+        message = """'cmd':'stop'"""
+        client.publish(self.mqtt_theme, message)
+        client.disconnect()
 
 if __name__ == '__main__':
     command = sys.argv
-    mqtt_ip = command[1]
-    mqtt_port = int(command[2])
-    mqtt_theme = command[3]
-    v = float(command[4])
-    omega = float(command[5])
-    file_path = command[6]
-
-    if not os.path.exists(file_path):
-        print("File not found")
-    else:
-        path = np.loadtxt(file_path, dtype=float)
-    bot_1 = Bot([path[0][0], path[0][1]], v, omega)
-    mqtt.Client.connected_flag = False
-    client = mqtt.Client("practice_1")
-    client.on_connect = on_connect
-    print("Connecting to brocker ", mqtt_ip)
-    client.connect(mqtt_ip, mqtt_port)
-    print("start loop")
-    message = """'cmd':'{}''val':'{}'"""
-    for i in range(1, len(path)):
-        bot_1.coords = path[i - 1]
-        df = bot_1.angle(path[i])
-        distance = bot_1.distance(path[i])
-        if -90 <= df <= 90:
-            if df > 0:
-                cmd = "left"
-                v = df/bot_1.omega
-                client.publish(message.format(cmd, v))
-                time.sleep(v)
-                bot_1.currentAngle += df
-            if df < 0:
-                cmd = "right"
-                v = abs(df/bot_1.omega)
-                client.publish(message.format(cmd, v))
-                time.sleep(v)
-                bot_1.currentAngle += df
-            cmd = "forward"
-            v = distance
-            client.publish(message.format(cmd, v))
-            time.sleep(v/bot_1.v)
-        else:
-
-            if df > 90 or df < -90:
-                df_reverse = bot_1.angle(-path[i])
-                if df_reverse > 0:
-                    cmd = "right"
-                    v = df_reverse/ bot_1.omega
-                    client.publish(message.format(cmd, v))
-                    time.sleep(v)
-                    bot_1.currentAngle += df
-                if df_reverse < 0:
-                    cmd = "left"
-                    v = df_reverse/ bot_1.omega
-                    client.publish(message.format(cmd, v))
-                    time.sleep(v)
-                    bot_1.currentAngle += df_reverse
-                cmd = "back"
-                v = distance
-                client.publish(mqtt_theme,message.format(cmd, v))
-                time.sleep(v / bot_1.v)
-
-    message = """'cmd':'stop'"""
-    client.publish(mqtt_theme, message)
-    client.disconnect()
-#python botcontrol.py 127.0.0.1 1883 abotcmd1 1.0 30.0 path.txt
+    bot_1 = Bot(float(command[4]), float(command[5]),command[1],int(command[2]),command[3],command[6])
+# python botcontrol.py 127.0.0.1 1883 abotcmd1 1.0 30.0 path.txt
