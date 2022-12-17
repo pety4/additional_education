4bc94c217aa06229c557fece85b94accc8b7b3be
Peter Orlov <peter_orl@mail.ru>
2022-12-17 16:04:36 +0300

    task_2

--- practice_4/task_2.py
+++ practice_4/task_2.py
@@ -3,12 +3,14 @@ with open("git_url.txt") as source:
     git_url = source.readlines()
 result={}
 for url in git_url:
+    url.rstrip()
     try:
         git.Repo.clone_from(url,f"{url[url.rfind('/')+1]}-https")
+        result[url] = 'OK'
     except:
         result[url]='FAIL'
-    result[url]='OK'
-with open("result.txt") as resultFile:
-    for i in result:
-        resultFile.write(i)
+with open("result.txt", "w") as resultFile:
+    for i in git_url:
+        resultFile.write(f"{i}\t")
+        resultFile.write(f"{result[i]}\n")
 
