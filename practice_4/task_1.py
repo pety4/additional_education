50b4cdbe223939bfb0b573fe8942a61fd16aeef1
Peter Orlov <peter_orl@mail.ru>
2022-12-17 15:14:44 +0300

    task_1

--- practice_4/task_1.py
+++ practice_4/task_1.py
@@ -1,7 +1,7 @@
-def evenNumberGenerator():
-    for i in range(2,100,2):
+def evenNumberGenerator(start,end):
+    for i in range(start,end+2,2):
         yield i
 if __name__ == '__main__':
-    value=evenNumberGenerator()
+    value=evenNumberGenerator(2,100)
     for i in value:
         print(i)
